ea72cd81c3540f80805296c4838bef85100952ca
yashvinamdeo <119424054+yashvinamdeo@users.noreply.github.com>
2022-12-03 21:26:39 +0530

    Add files via upload

---
+++ area.c
@@ -0,0 +1,15 @@
+//To find the area of circle
+#include<stdio.h>
+
+int main()
+{
+float radius,area;
+
+printf("\nEnter the value of radius:");
+scanf("%f",&radius);
+
+area=3.14*radius*radius;
+printf("The area of the circle for given radius is %f",area);
+
+return 0;
+}
