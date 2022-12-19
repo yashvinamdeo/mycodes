96c31f363b0c89ccfd41cfe47ca97bed8749348d
yashvinamdeo <119424054+yashvinamdeo@users.noreply.github.com>
2022-12-20 00:46:48 +0530

    Update area.c

--- area.c
+++ area.c
@@ -6,9 +6,9 @@ int main()
 float radius,area;
 
 printf("\nEnter the value of radius:");
-scanf("%f",&radius);
+scanf("%f",&radius);//Enter the value of radius
 
-area=3.14*radius*radius;
+area=3.14*radius*radius;//Formula for area of circle
 printf("The area of the circle for given radius is %f",area);
 
 return 0;
